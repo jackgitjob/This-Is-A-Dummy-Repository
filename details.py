b08912474b5a091e602c5906c8fa5c439d80bfa2
Jack Lai <151252240+jackgitjob@users.noreply.github.com>
2023-12-10 17:12:03 +0000

    Eliminated all PEP 8 warnings

--- details.py
+++ details.py
@@ -13,38 +13,38 @@ convert string1 into an integer and assign to string1_int
 print string1_int
 """
 
-#assign 99.23 to num1
+# assign 99.23 to num1
 num1 = 99.23
 
-#convert num1 into an integer and assign to num1_int
+# convert num1 into an integer and assign to num1_int
 num1_int = int(num1)
 
-#print num1_str
+# print num1_str
 print(f"num1 = {num1_int}")
 
-#assign 23 to num2
+# assign 23 to num2
 num2 = 23
 
-#convert num2 into a float and assign to num2_float
+# convert num2 into a float and assign to num2_float
 num2_float = float(num2)
 
-#print num1_str
+# print num1_str
 print(f"num2 = {num2_float}")
 
-#assign 150 to num3
+# assign 150 to num3
 num3 = 150
 
-#convert num3 into a string and assign to num3_str
+# convert num3 into a string and assign to num3_str
 num3_str = str(num3)
 
-#print num3_str
+# print num3_str
 print(f"num3 = {num3_str}")
 
-#assign "100" to string1
+# assign "100" to string1
 string1 = "100"
 
-#convert string1 into an integer and assign to string1_int
+# convert string1 into an integer and assign to string1_int
 string1_int = int(string1)
 
-#print string1_int
+# print string1_int
 print(f"string1 = {string1_int}")
