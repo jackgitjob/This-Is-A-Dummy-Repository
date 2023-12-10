505511c4139a64f70fcb5981e4ab9ed5b7c927bd
jackgitjob <151252240+jackgitjob@users.noreply.github.com>
2023-12-10 16:54:19 +0000

    Create details.py

---
+++ details.py
@@ -0,0 +1,50 @@
+"""
+assign 99.23 to num1
+convert num1 into an integer and assign to num1_int
+print num1_int
+assign 23 to num2
+convert num2 into a float and assign to num2_float
+print num2_float
+assign 150 to num3
+convert num3 into a string and assign to num3_str
+print num3_str
+assign "100" to string1
+convert string1 into an integer and assign to string1_int
+print string1_int
+"""
+
+#assign 99.23 to num1
+num1 = 99.23
+
+#convert num1 into an integer and assign to num1_int
+num1_int = int(num1)
+
+#print num1_str
+print(f"num1 = {num1_int}")
+
+#assign 23 to num2
+num2 = 23
+
+#convert num2 into a float and assign to num2_float
+num2_float = float(num2)
+
+#print num1_str
+print(f"num2 = {num2_float}")
+
+#assign 150 to num3
+num3 = 150
+
+#convert num3 into a string and assign to num3_str
+num3_str = str(num3)
+
+#print num3_str
+print(f"num3 = {num3_str}")
+
+#assign "100" to string1
+string1 = "100"
+
+#convert string1 into an integer and assign to string1_int
+string1_int = int(string1)
+
+#print string1_int
+print(f"string1 = {string1_int}")
